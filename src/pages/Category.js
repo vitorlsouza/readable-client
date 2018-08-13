0d8e4fb5c102079b4621e1c9acdf8d8c0c1ae391
Vitor <vitorlopesdesouza@gmail.com>
2018-08-13 17:09:53 -0300

    update: loading update categories

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import IconButton from 'material-ui/IconButton';
 import FontIcon from 'material-ui/FontIcon';
 
+import gifLoading from '../assets/img/loading.svg';
+
 import FadeIn from 'react-fade-in';
 
 /* Redux */
@@ -58,77 +60,87 @@ class Category extends Component {
   };
 
   render() {
-    return (
-      <div className="content">
-        {this.state.loading ? (
-          <p>Loading Categories</p>
-        ) : (
-          <FadeIn>
-            <Grid>
-              {this.props.postsCategory.map(post => (
-                <Row key={post.id}>
-                  <Col xs={12} sm={12} md={9} lg={9}>
-                    <Panel>
-                      <Panel.Heading>
-                        <Panel.Title>
-                          <Row>
-                            <Col xs={10} md={10}>
-                              {post.title}
-                            </Col>
-                            <Col xs={2} md={2}>
-                              <Link to={`${post.category}/${post.id}`}>
-                                Details
-                              </Link>
-                            </Col>
-                          </Row>
-                        </Panel.Title>
-                      </Panel.Heading>
-                      <Panel.Body>{post.body}</Panel.Body>
-                      <Panel.Footer>
-                        <Col xs={5}>
-                          <p style={{ fontSize: 20, verticalAlign: 'middle' }}>
-                            {post.commentCount} Comments
-                          </p>
-                        </Col>
-                        <Col xsOffset={11}>
-                          <IconButton
-                            onClick={() => this.voteScorePostUp(post.id)}
-                            style={{ marginBottom: -20 }}
-                            tooltip="vote Up"
-                            tooltipPosition="top-center"
-                          >
-                            <FontIcon className="fa fa-sort-up" />
-                          </IconButton>
-                          <p
-                            style={{
-                              fontSize: 27,
-                              marginTop: -10,
-                              marginBottom: -3,
-                              marginRight: this.state.width <= 1199 ? 6 : 20,
-                              textAlign: 'center',
-                            }}
-                          >
-                            {post.voteScore}
-                          </p>
-                          <IconButton
-                            onClick={() => this.voteScorePostDown(post.id)}
-                            style={{ marginTop: -20 }}
-                            tooltip="vote Down"
-                            tooltipPosition="bottom-center"
-                          >
-                            <FontIcon className="fa fa-sort-down" />
-                          </IconButton>
-                        </Col>
-                      </Panel.Footer>
-                    </Panel>
-                  </Col>
-                </Row>
-              ))}
-            </Grid>
-          </FadeIn>
-        )}
-      </div>
-    );
+    {
+      if (this.state.loading)
+        return (
+          <div className="loading">
+            <img src={gifLoading} className="gif" alt="gif loading" />
+          </div>
+        );
+      return (
+        <div className="content">
+          {this.state.loading ? (
+            <img src={gifLoading} alt="gif loading" />
+          ) : (
+            <FadeIn>
+              <Grid>
+                {this.props.postsCategory.map(post => (
+                  <Row key={post.id}>
+                    <Col xs={12} sm={12} md={9} lg={9}>
+                      <Panel>
+                        <Panel.Heading>
+                          <Panel.Title>
+                            <Row>
+                              <Col xs={10} md={10}>
+                                {post.title}
+                              </Col>
+                              <Col xs={2} md={2}>
+                                <Link to={`${post.category}/${post.id}`}>
+                                  Details
+                                </Link>
+                              </Col>
+                            </Row>
+                          </Panel.Title>
+                        </Panel.Heading>
+                        <Panel.Body>{post.body}</Panel.Body>
+                        <Panel.Footer>
+                          <Col xs={5}>
+                            <p
+                              style={{ fontSize: 20, verticalAlign: 'middle' }}
+                            >
+                              {post.commentCount} Comments
+                            </p>
+                          </Col>
+                          <Col xsOffset={11}>
+                            <IconButton
+                              onClick={() => this.voteScorePostUp(post.id)}
+                              style={{ marginBottom: -20 }}
+                              tooltip="vote Up"
+                              tooltipPosition="top-center"
+                            >
+                              <FontIcon className="fa fa-sort-up" />
+                            </IconButton>
+                            <p
+                              style={{
+                                fontSize: 27,
+                                marginTop: -10,
+                                marginBottom: -3,
+                                marginRight: this.state.width <= 1199 ? 6 : 20,
+                                textAlign: 'center',
+                              }}
+                            >
+                              {post.voteScore}
+                            </p>
+                            <IconButton
+                              onClick={() => this.voteScorePostDown(post.id)}
+                              style={{ marginTop: -20 }}
+                              tooltip="vote Down"
+                              tooltipPosition="bottom-center"
+                            >
+                              <FontIcon className="fa fa-sort-down" />
+                            </IconButton>
+                          </Col>
+                        </Panel.Footer>
+                      </Panel>
+                    </Col>
+                  </Row>
+                ))}
+              </Grid>
+            </FadeIn>
+          )}
+        </div>
+      );
+    }
   }
 }
 
