8e900296a3e5d33c3d352548ab16b9b6eec410e8
Vitor <vitorlopesdesouza@gmail.com>
2018-08-13 15:14:02 -0300

    update: const api from ApiHelper.js

--- src/utils/ApiHelper.js
+++ src/utils/ApiHelper.js
@@ -1,125 +1,127 @@
-const api = "http://localhost:3001";
+const api = 'https://server-readable.herokuapp.com';
 
-const headers = { 'Authorization': 'whatever-you-want', 'Content-Type': 'application/json' };
+const headers = {
+  Authorization: 'whatever-you-want',
+  'Content-Type': 'application/json',
+};
 
 export const getAllCategories = async () => {
   const response = await fetch(`${api}/categories`, { headers })
     .then(res => res.json())
-    .then(data => data.categories)
-    return response
-}
+    .then(data => data.categories);
+  return response;
+};
 
 export const getAllPosts = async () => {
   const response = await fetch(`${api}/posts`, { headers })
     .then(res => res.json())
-    .then(data => data)
-    return response
-}
+    .then(data => data);
+  return response;
+};
 
-export const getPostByCategory = async (category) => {
+export const getPostByCategory = async category => {
   const response = await fetch(`${api}${category}/posts`, { headers })
     .then(res => res.json())
-    .then(data => data)
-    return response
-}
+    .then(data => data);
+  return response;
+};
 
-export const getPostById = async (id) => {
+export const getPostById = async id => {
   const response = await fetch(`${api}/posts/${id}`, { headers })
-  .then(res => res.json())
-  .then(data => data)
-  return response
-}
+    .then(res => res.json())
+    .then(data => data);
+  return response;
+};
 
-export const getAllCommentsByPost = async (id) => {
+export const getAllCommentsByPost = async id => {
   const response = await fetch(`${api}/posts/${id}/comments`, { headers })
-  .then(res => res.json())
-  .then(data => data)
-  return response
-}
+    .then(res => res.json())
+    .then(data => data);
+  return response;
+};
 
-export const removePost = async (id) => {
-  const response = await fetch(`${api}/posts/${id}`,
-  {
+export const removePost = async id => {
+  const response = await fetch(`${api}/posts/${id}`, {
     headers,
-    method: 'DELETE'
+    method: 'DELETE',
   })
-  .then(res => res.json())
-  .then(data => data)
-  return response
-}
+    .then(res => res.json())
+    .then(data => data);
+  return response;
+};
 
-export const addPost = async (post) => {
+export const addPost = async post => {
   const response = await fetch(`${api}/posts`, {
     headers,
     method: 'POST',
-    body: JSON.stringify(post)
+    body: JSON.stringify(post),
   })
-  .then(res => res.json())
-  .then(data => data)
-  return response
-}
+    .then(res => res.json())
+    .then(data => data);
+  return response;
+};
 
 export const editPost = async (post, id) => {
   const response = await fetch(`${api}/posts/${id}`, {
     headers,
-    method: "PUT",
-    body: JSON.stringify(post)
+    method: 'PUT',
+    body: JSON.stringify(post),
   })
-  .then(res => res.json())
-  .then(data => data)
-  return response
-}
+    .then(res => res.json())
+    .then(data => data);
+  return response;
+};
 
-export const addComment = async (comment) => {
+export const addComment = async comment => {
   const response = await fetch(`${api}/comments`, {
     headers,
-    method: "POST",
-    body: JSON.stringify(comment)
+    method: 'POST',
+    body: JSON.stringify(comment),
   })
-  .then(res => res.json())
-  .then(data => data)
-  return response
-}
+    .then(res => res.json())
+    .then(data => data);
+  return response;
+};
 
 export const editComment = async (comment, id) => {
   const response = await fetch(`${api}/comments/${id}`, {
     headers,
-    method: "PUT",
-    body: JSON.stringify(comment)
+    method: 'PUT',
+    body: JSON.stringify(comment),
   })
-  .then(res => res.json())
-  .then(data => data)
-  return response
-}
+    .then(res => res.json())
+    .then(data => data);
+  return response;
+};
 
-export const removeComment = async (id) => {
+export const removeComment = async id => {
   const response = await fetch(`${api}/comments/${id}`, {
     headers,
-    method: "DELETE"
+    method: 'DELETE',
   })
-  .then(res => res.json())
-  .then(data => data)
-  return response
-}
+    .then(res => res.json())
+    .then(data => data);
+  return response;
+};
 
 export const votePost = async (id, option) => {
   const response = await fetch(`${api}/posts/${id}`, {
     headers,
-    method: "POST",
-    body: JSON.stringify({option})
+    method: 'POST',
+    body: JSON.stringify({ option }),
   })
-  .then(res => res.json())
-  .then(data => data)
-  return response
-}
+    .then(res => res.json())
+    .then(data => data);
+  return response;
+};
 
 export const voteComment = async (id, option) => {
   const response = await fetch(`${api}/comments/${id}`, {
     headers,
-    method: "POST",
-    body: JSON.stringify({option})
+    method: 'POST',
+    body: JSON.stringify({ option }),
   })
-  .then(res => res.json())
-  .then(data => data)
-  return response
-}
+    .then(res => res.json())
+    .then(data => data);
+  return response;
+};
